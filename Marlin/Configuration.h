a24dece7ff8c5eba395aea90d2d8dbf84c170ca2
Mateusz Bronk <mbronk@users.noreply.github.com>
2020-06-21 22:15:18 +0200

    Adding custom SapphirePRO GH build workflow
    Updated status badge in README.md
    Add automated build for various UIs

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2764,6 +2764,19 @@
 //#define TFT_CLASSIC_UI
 //#define TFT_COLOR_UI
 //#define TFT_LVGL_UI
+#ifndef UI_SELECTION  //for building 2 UIs via platformIO
+  #define UI_SELECTION 1
+#endif
+
+#if UI_SELECTION==1
+  #define TFT_COLOR_UI //@@SapphirePro - Classic Marlin menu-driven UI
+#elif UI_SELECTION==2
+  #define TFT_LVGL_UI
+#elif UI_SELECTION==3
+  #define TFT_CLASSIC_UI
+#else
+  #error "Select UI [1|2|3] -DUI_SELECTION=1"
+#endif
 
 #if ENABLED(TFT_LVGL_UI)
   //#define MKS_WIFI_MODULE  // MKS WiFi module
