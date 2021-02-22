c0fce1a4f4350b2b4e3bc2c6bedf5fcdb68daf50
Mateusz Bronk <bronk.m@gmail.com>
2021-02-22 09:51:32 +0100

    Added emergency parser

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1968,7 +1968,7 @@
  * Currently handles M108, M112, M410, M876
  * NOTE: Not yet implemented for all platforms.
  */
-//#define EMERGENCY_PARSER //@@? - not yet implemented for STM32F1 ***************
+#define EMERGENCY_PARSER //@@? - not yet implemented for STM32F1 ***************
 
 // Bad Serial-connections can miss a received command by sending an 'ok'
 // Therefore some clients abort after 30 seconds in a timeout.
